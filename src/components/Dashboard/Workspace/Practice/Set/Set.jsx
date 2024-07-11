07f0f750200312376631dd5bcb629f9c79b86de3
Rona <longhairshortcode@gmail.com>
2024-07-10 23:53:38 -0700

    everything fixed except when on consonant and press vowel, vowel's yellow isnt the fc showing, and need to put reading fc in div to make them rows

--- src/components/Dashboard/Workspace/Practice/Set/Set.jsx
+++ src/components/Dashboard/Workspace/Practice/Set/Set.jsx
@@ -142,7 +142,7 @@ const numberPracticeFactId = Number(practiceFactId)
               </button>
             ))}
           </div>
-          <div className={style.setsssContainer}>
+          <div className={style.setssContainer}>
             {filteredFlashcards.map((flashcard, index) => (
                 <div className={style.singleCard} key={index}>
                   <div className={style.flipCard}>
