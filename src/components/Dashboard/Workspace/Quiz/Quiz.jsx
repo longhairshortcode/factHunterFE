eecfa9622225a284349742e1bd3fd085752c9050
Rona <longhairshortcode@gmail.com>
2024-07-12 21:55:28 -0700

    started styling quiz

--- src/components/Dashboard/Workspace/Quiz/Quiz.jsx
+++ src/components/Dashboard/Workspace/Quiz/Quiz.jsx
@@ -1,16 +1,54 @@
+
+import { useState } from "react";
 import style from "./Quiz.module.css"
 
 
 function Quiz() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  function handleCategoryClick(category) {
+    setSelectedCategory(category);
+  } 
+
+
   return (
     <div className={style.componentContainer}>
       <div className={style.titleAndButtonsContainer}>
         <p className={style.title}>Quiz</p>
+        <div className={style.mathAndReadingButtonContainer}>
+          <button
+            className={style.mathQuizButton}
+            onClick={() => handleCategoryClick("math")}
+          >
+            Math Quizzes
+          </button>
+          <button
+            className={style.readingQuizButton}
+            onClick={() => handleCategoryClick("reading")}
+          >
+            Reading Quizzes
+          </button>
+        </div>
 
+        <div className={style.categoriesContainer}>
+          {selectedCategory === "math" && (
+            <div>
+              <p>Addition</p>
+              <p>Subtraction</p>
+              <p>Multiplication</p>
+              <p>Division</p>
+            </div>
+          )}
+          {selectedCategory === "reading" && (
+            <div>
+              <p>Vowels</p>
+              <p>Consonants</p>
+            </div>
+          )}
+        </div>
       </div>
-    
     </div>
-  )
+  );
 }
 
-export default Quiz
+export default Quiz;
