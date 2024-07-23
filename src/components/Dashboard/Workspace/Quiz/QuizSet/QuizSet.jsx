b6d84831659e1ed24e48a461739485ccdf619681
Rona <longhairshortcode@gmail.com>
2024-07-22 18:45:50 -0700

    got colors to change based on incorrect or correct and when click save results, quizSet clears and can choose a new fact quiz

--- src/components/Dashboard/Workspace/Quiz/QuizSet/QuizSet.jsx
+++ src/components/Dashboard/Workspace/Quiz/QuizSet/QuizSet.jsx
@@ -39,7 +39,7 @@ function nextQuestion() {
 }
 
 //if grabbing more than 1, [] destructure, if 1, {} destructure
-asfunction handleChange(e){
+assfunction handleChange(e){
   const {value} = e.target
   setAnswer(value)
 }
